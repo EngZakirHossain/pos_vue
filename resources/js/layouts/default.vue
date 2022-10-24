de7b778c26b977f93c30e0241e9a697aa06dee0f
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-24 10:30:59 +0600

    first commit

---
+++ resources/js/layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <navbar />
+    <sidebar />
+    <!-- Main content -->
+    <section class="content-wrapper">
+      <div class="container-fluid page-padding">
+        <child />
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <footer class="main-footer">
+      <strong v-if="appInfo">{{ appInfo.copyright }}.</strong>
+      Developed by
+      <a href="https://mrzakir.com/" target="__blank">Zakir Hossain</a>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <sidebar-controll />
+    <!-- /.control-sidebar -->
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Navbar from '~/components/Navbar'
+import Sidebar from '~/components/Sidebar'
+import SidebarControll from '~/components/SidebarControll'
+export default {
+  name: 'MainLayout',
+  data: () => ({
+    year: new Date().getFullYear(),
+  }),
+  components: {
+    Navbar,
+    Sidebar,
+    SidebarControll,
+  },
+  // Map Getters
+  computed: {
+    ...mapGetters('operations', ['appInfo']),
+  },
+  methods: {
+    addBodyClass(className) {
+      document.body.classList.toggle(className)
+    },
+  },
+}
+</script>
